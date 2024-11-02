a04f926e25051c2a79ff81a6143b5dbcdd6c3adf
newbieJanghan <bjh4135@gmail.com>
2024-11-02 22:23:15 +0900

    doc: init swagger for api doc

---
+++ src/main/java/com/ssafy/housework/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.ssafy.housework.config;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+            .title("Housework REST API")
+            .description("Housework API for SSAFY 12th final project")
+            .version("0.0.1");
+    }
+}
