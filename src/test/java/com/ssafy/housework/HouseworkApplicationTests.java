5de8a827e9d2fb6acb2c39e719bb0d69f9abaee1
newbieJanghan <bjh4135@gmail.com>
2024-11-02 21:04:06 +0900

    start

---
+++ src/test/java/com/ssafy/housework/HouseworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.housework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
