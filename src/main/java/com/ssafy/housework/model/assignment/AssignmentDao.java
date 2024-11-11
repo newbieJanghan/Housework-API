e03839e9f53add0ebe89de95dd799541209194fb
newbieJanghan <bjh4135@gmail.com>
2024-11-11 16:55:38 +0900

    feat: assignment service

--- src/main/java/com/ssafy/housework/model/assignment/AssignmentDao.java
+++ src/main/java/com/ssafy/housework/model/assignment/AssignmentDao.java
@@ -1,4 +1,19 @@
 package com.ssafy.housework.model.assignment;
 
+import com.ssafy.housework.model.assignment.dto.Assignment;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
 public interface AssignmentDao {
+    List<Assignment> selectAllByTaskId(int taskId);
+
+    Assignment selectOne(int id);
+
+    int insert(Assignment assignment);
+
+    int update(Assignment assignment);
+
+    int delete(int id);
 }
---
+++ src/main/java/com/ssafy/housework/model/assignment/dto/AssignmentUpdate.java
@@ -0,0 +1,5 @@
+package com.ssafy.housework.model.assignment.dto;
+
+public record AssignmentUpdate(int calorieBurned) {
+
+}
