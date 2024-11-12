7e0968cdbb3b28eda9ecad1a897ebf5d172b5213
newbieJanghan <bjh4135@gmail.com>
2024-11-12 14:58:04 +0900

    UPDATE: schema v1.0

--- src/main/resources/db/init.sql
+++ src/main/resources/db/init.sql
@@ -16,87 +16,48 @@ CREATE TABLE users
 (
     id        INT AUTO_INCREMENT PRIMARY KEY,
     family_id INT NOT NULL,
+
     name      VARCHAR(50) NOT NULL,
     email     VARCHAR(50) NOT NULL UNIQUE,
     password  VARCHAR(255) NOT NULL,
-    profile_image_url TEXT,
+    family_role VARCHAR(255),
+    profile_image_name TEXT,
+    calorie_goal INT DEFAULT 0,
+
     is_admin BOOLEAN DEFAULT FALSE,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (family_id) REFERENCES families (id)
 );
 
-CREATE TABLE family_members
-(
-    id        INT AUTO_INCREMENT PRIMARY KEY,
-    user_id   INT          NOT NULL,
-    family_id INT          NOT NULL,
-    role      VARCHAR(50) NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users (id),
-    FOREIGN KEY (family_id) REFERENCES families (id)
-);
-
-CREATE TABLE housework_categories
-(
-    id   INT AUTO_INCREMENT PRIMARY KEY,
-    family_id INT NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
-
 CREATE TABLE houseworks
 (
     id       INT AUTO_INCREMENT PRIMARY KEY,
     family_id INT NOT NULL,
-    housework_category_id INT COMMENT "nullable if not specified",
+    register_user_id INT NOT NULL,
+    assigned_user_id INT NOT NULL,
+
     name     VARCHAR(255) NOT NULL,
-    calorie_amount INT,
+    description TEXT,
+    color VARCHAR(50) DEFAULT "#000000",
+
+    start_at DATETIME NOT NULL,
+    due_at  DATETIME NOT NULL,
+    done_at DATETIME COMMENT "if null, not done",
+
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (family_id) REFERENCES families (id),
-    FOREIGN KEY (housework_category_id) REFERENCES housework_categories (id)
+    FOREIGN KEY (register_user_id) REFERENCES users (id),
+    FOREIGN KEY (assigned_user_id) REFERENCES users (id)
 );
 
-CREATE TABLE tasks
-(
-    id               INT AUTO_INCREMENT PRIMARY KEY,
-    family_id        INT      NOT NULL,
-    housework_id     INT      NOT NULL,
-    name             VARCHAR(255),
-    description      TEXT,
-    due_at           DATETIME NOT NULL,
-    done_at   DATETIME COMMENT "if null, not done",
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (family_id) REFERENCES families (id),
-    FOREIGN KEY (housework_id) REFERENCES houseworks (id)
-);
-
-CREATE TABLE assignments
+CREATE TABLE daily_workouts
 (
     id        INT AUTO_INCREMENT PRIMARY KEY,
-    family_member_id   INT NOT NULL,
-    task_id   INT NOT NULL,
-    calorie_burned INT NOT NULL COMMENT "FROM housework",
-    is_active BOOLEAN DEFAULT TRUE COMMENT "if false, soft deleted",
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (task_id) REFERENCES tasks (id),
-    FOREIGN KEY (family_member_id) REFERENCES families (id)
-);
-
-CREATE TABLE workout_campaigns
-(
-    id        INT AUTO_INCREMENT PRIMARY KEY,
-    family_member_id   INT NOT NULL,
-    calorie_goal    INT NOT NULL,
+    user_id  INT NOT NULL,
     calorie_burned  INT NOT NULL,
-    start_at        DATETIME NOT NULL,
-    end_at          DATETIME NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (family_member_id) REFERENCES families (id)
+    FOREIGN KEY (user_id) REFERENCES users (id)
 )
