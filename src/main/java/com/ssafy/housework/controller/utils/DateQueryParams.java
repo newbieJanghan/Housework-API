7c6d2c4a4e7e403e2f0ebf9ef320a13cdfcb1f13
newbieJanghan <bjh4135@gmail.com>
2024-11-25 13:02:13 +0900

    UPDATE: DateQueryParams set default on today

--- src/main/java/com/ssafy/housework/controller/utils/DateQueryParams.java
+++ src/main/java/com/ssafy/housework/controller/utils/DateQueryParams.java
@@ -3,4 +3,8 @@ package com.ssafy.housework.controller.utils;
 import java.time.LocalDateTime;
 
 public record DateQueryParams(LocalDateTime from, LocalDateTime to) {
+    public DateQueryParams {
+        from = LocalDateTime.now().withHour(0).withMinute(0).withSecond(0).withNano(0);
+        to = LocalDateTime.now().withHour(23).withMinute(59).withSecond(59).withNano(999);
+    }
 }
